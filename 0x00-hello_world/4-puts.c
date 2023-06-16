004eb3f6d6d59797fbba817b08a7910c6b2962ee
Super User <kawtarennabirha@gmail.com>
2023-06-16 12:13:41 -0700

    ddd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,7 @@
-#!/bin/bash
-/*
 #include <stdio.h>
-#include <string.h>
-int main () {
-char str1[100];
-strcpy(str1,"/"Programming is like building a multilingual puzzle );
-puts(str);
-return(0);
-}
-*/
+
+int main ()
+{ 
+	puts ("Programming is like building a multilingual puzzle " );
+	return(0);
+}	
