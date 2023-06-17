f6d6dbc2b41b57a6fb2d28378e2e448eb75f98f9
Super User <kawtarennabirha@gmail.com>
2023-06-17 05:43:38 -0700

    ddd

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,35 +2,34 @@
 /**
  * main-enty point
  *
- * return: always 0(success)
+ * description: Positive anything is better than negative nothing
+ *
+ * Return: Always 0(Success)
 */
 int main(void)
 
 {
 	int n;
 
-	scanf("%d ", &n);
+	scanf("%d", &n);
 
 	if (n < 0)
 
 	{
-		printf("is negative\n");
+		printf("the number is negative.\n");
 
 	}
 else
 
 
 {
+	if (n == 0)
 
-	if (n == 0);
-
-
-	printf("is 0\n");
-
+	printf("the number is 0.\n");
 }
 
-printf("is positive\n");
+
+printf("the number is positive.\n");
 
 return (0);
-
 }
