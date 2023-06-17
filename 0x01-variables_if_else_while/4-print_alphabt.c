64e3b6aaa4802bee97e503fc5be4395a189a2945
Super User <kawtarennabirha@gmail.com>
2023-06-17 13:49:01 -0700

    dd

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,17 +8,12 @@ int main(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'd'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
+		if (c != 'q' && c != 'e')
+		{
 		putchar(c);
-	}
-	for (c = 'f'; c <= 'p'; c++)
-	{
-		putchar(c);
-	}
-	for (c = 'r'; c <= 'z'; c++)
-	{
-		putchar(c);
+		}
 	}
 	putchar ('\n');
 	return (0);
