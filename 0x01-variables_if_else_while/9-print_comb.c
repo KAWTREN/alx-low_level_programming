8a4d72b55612dbaaced071e93fb3238372b2c553
Super User <kawtarennabirha@gmail.com>
2023-06-18 02:48:13 -0700

    ss

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
