6fe180c500a4b6a8872ac8cf458b1e300005a994
Super User <kawtarennabirha@gmail.com>
2023-06-20 14:29:48 -0700

    dd

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * jack_bauer-prints every minute of the day of Jack Bauer
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + 0);
+		_putchar('0' + 0);
+		_putchar(':');
+		_putchar('0' + 0);
+		_putchar('0' + i);
+		_putchar('\n');
+	}
+	for (i = 0; j <= 9; j++)
+	{
+		_putchar('0' + 2);
+		_putchar('0' + 3);
+		_putchar(':');
+		_putchar('0' + 5);
+		_putchar('0' + j);
+		_putchar('\n');
+	}
+
+
+
+}
