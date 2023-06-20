691a7e57051bde8409d27176729bd906e824983a
Super User <kawtarennabirha@gmail.com>
2023-06-20 15:41:40 -0700

    dd

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,19 +13,20 @@ void jack_bauer(void)
 		for (j = 0; j <= 9; j++)
 		{
 			if ((i <= 1 && j <= 9) || (i <= 2 && j <= 3))
-					{
-					for (k = 0; k <= 5; k++)
-					{
+			{
+				for (k = 0; k <= 5; k++)
+				{
 					for (l = 0; l <= 9; l++)
-					{_putchar('0' + i);
-					_putchar('0' + j);
-					_putchar(':');
-					_putchar('0' + k);
-					_putchar('0' + l);
-					_putchar('\n');
+					{
+						_putchar('0' + i);
+						_putchar('0' + j);
+						_putchar(':');
+						_putchar('0' + k);
+						_putchar('0' + l);
+						_putchar('\n')
 					}
-					}
-					}
+				}
+			}
 		}
 	}
 
