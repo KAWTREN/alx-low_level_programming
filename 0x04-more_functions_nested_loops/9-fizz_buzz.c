10d97fd51e1f089604b1b8eb57b7f1e6f9596386
Super User <kawtarennabirha@gmail.com>
2023-06-22 09:20:34 -0700

    r

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main-rints the numbers from 1 to 100
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 1; i <= 100; i++)
+	{
+		j = i % 3;
+		k = i % 5;
+		if (j == 0 && k == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (k == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (j == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i == 100)
+		{
+			printf("%d\n", i);
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
