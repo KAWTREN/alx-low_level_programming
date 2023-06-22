34fd894369399f6bc2ecec2ab122c08d758865b0
Super User <kawtarennabirha@gmail.com>
2023-06-22 03:53:52 -0700

    r

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,11 @@ void print_most_numbers(void)
 
 	while (i <= 9)
 	{
-		if (i != 2 || i != 4)
+		if ((i != 2) && (i != 4))
 		{
 			_putchar('0' + i);
-			i++;
 		}
+		i++;
 	}
 	_putchar('\n');
 }
