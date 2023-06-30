6b4eb78cbe0b9960915d38871808a8b539a9c4eb
Super User <kawtarennabirha@gmail.com>
2023-06-30 10:50:25 -0700

    rrr

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,19 +4,26 @@
  * @dest: string
  * @src: string
  * @n: integer
+ * Return: char
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-
-	while (i < n && src[i] != '\0')
+	
+	if (dest == 0 || src == 0 || n <= 0)
+        return dest;
+	else
 	{
-		dest[i] = src[i];
-		i++;
-	}
-	while (i < n)
-	{
-		dest[i] = '\0';
+		while (i < n && src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		while (i < n)
+		{
+			dest[i] = '\0';
+			i++;
+		}
 	}
 	return (dest);
 }
