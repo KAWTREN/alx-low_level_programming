2b026adae34ab0cff39a946aebd6320631d5ddda
Super User <kawtarennabirha@gmail.com>
2023-07-01 04:16:27 -0700

    rr

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_number-prints an integer
+ * @n: integer
+ */
+void print_number(int n)
+{
+	int divisor, digit;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	divisor = 1;
+	while (n / divisor >= 10)
+	{
+		divisor *= 10;
+	}
+	while (divisor != 0)
+	{
+		digit = n / divisor;
+		_putchar('0' + digit);
+		n %= divisor;
+		divisor /= 10;
+	}
+}
