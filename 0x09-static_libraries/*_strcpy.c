6141f5cd9d6c7705f7ac9cbac9d38abea78c627e
Super User <kawtarennabirha@gmail.com>
2023-07-07 10:12:52 -0700

    rr

---
+++ 0x09-static_libraries/*_strcpy.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+/**
+ * _strcpy- copies the string
+ * @dest: string1
+ * @src: string2
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
