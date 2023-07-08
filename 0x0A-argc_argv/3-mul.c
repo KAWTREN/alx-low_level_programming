80265e41ca446edf0653eda10d66ac745d173af0
Super User <kawtarennabirha@gmail.com>
2023-07-08 05:39:17 -0700

    rr

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-multiplies two numbers
+ * @argc:integer
+ * @argv: array
+ * Return:0
+ */
+int main(int argc, char **argv)
+{
+	int k = 1;
+
+	if (argc == 1 || argc == 2)
+	{
+		printf("error\n");
+		return (1);
+	}
+	else
+	{
+		k = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", k);
+	}
+	return (0);
+}
