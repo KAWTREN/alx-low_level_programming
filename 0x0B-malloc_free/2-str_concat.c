a3f1b0306c675ec1a903fd0fef7df542f7f2fe2b
Super User <kawtarennabirha@gmail.com>
2023-07-11 15:51:58 -0700

    ttt

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,33 +1,30 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "main.h"
 /**
- * *str_concat-concatenates two strings
- * @s1:string1
- * @s2:string2
- * Return: char
+ * str_concat - concatenates two strings
+ * @s1: input string 1
+ * @s2: input string 2
+ * Return: pointer to newly allocated space in memory with both strings or NULL
  */
 char *str_concat(char *s1, char *s2)
 {
-	int len1, len2;
-	char *result;
+	char *pstr;
+	unsigned int len1 = 0, len2 = 0, i, j;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	result = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (result == NULL)
-		return (NULL);
-	if (s1 != NULL)
-	{
-		strcpy(result, s1);
-	}
-	if (s2 != NULL)
-	{
-		strcat(result, s2);
-	}
-	return (result);
-
-
+	if (s1 == 0)
+		s1 = "";
+	if (s2 == 0)
+		s2 = "";
+	while (s1[len1] != 0)
+		len1++;
+	while (s2[len2] != 0)
+		len2++;
+	len2++;
+	pstr = malloc(sizeof(char) * (len1 + len2));
+	for (i = 0; i < len1; i++)
+		pstr[i] = s1[i];
+	for (j = 0; j < len2; i++, j++)
+		pstr[i] = s2[j];
+	return (pstr);
 }
