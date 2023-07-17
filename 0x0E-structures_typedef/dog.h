81576b6885c0aa63ee5b016bbc3890159947d553
Super User <kawtarennabirha@gmail.com>
2023-07-17 13:36:56 -0700

    rr

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
-struct dog
+#ifndef _dog_h
+#define _dog_h
+
+/**
+ * struct dog-dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+#endif /* dog_h */
