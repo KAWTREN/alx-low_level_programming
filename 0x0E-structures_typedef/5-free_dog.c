7cd4e741353b44c564d97500f966ccc71389f7d5
Super User <kawtarennabirha@gmail.com>
2023-07-18 08:14:13 -0700

    rr

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -6,9 +6,11 @@
  */
 void free_dog(dog_t *d)
 {
-    if (d == NULL)
-        return;
-    free(d->name);
-    free(d->owner);
-    free(d);
+	if (d == NULL)
+	{
+		return;
+	}
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
