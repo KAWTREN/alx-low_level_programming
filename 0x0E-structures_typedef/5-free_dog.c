52280c620cccd1be13f3c53a3c89cdb857a3af66
Super User <kawtarennabirha@gmail.com>
2023-07-18 08:11:02 -0700

    rr

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog-free dogs
+ * @d: dog_t
+ */
+void free_dog(dog_t *d)
+{
+    if (d == NULL)
+        return;
+    free(d->name);
+    free(d->owner);
+    free(d);
+}
