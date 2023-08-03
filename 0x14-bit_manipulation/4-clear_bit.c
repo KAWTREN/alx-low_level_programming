dad4f29c89ef37823854481b69037ec5c9112e79
KAWTREN <kawtarennabirha@g,ail.com>
2023-08-03 13:05:22 -0700

    bits

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,7 +9,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int m = 1UL << index;
 
-	if (index >>= sizeof(unsigned long int) * 8)
+	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
