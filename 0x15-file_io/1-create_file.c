7c674421fbed4ad39dbacfe9e1353d5eec633ef1
KAWTREN <kawtarennabirha@g,ail.com>
2023-08-07 13:40:40 -0700

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,12 +21,9 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	byte = write(fd, text_content, len);
+	if (fd == -1 || byte == -1)
 		return (-1);
-	if (len)
-	{
-		byte = write(fd, text_content, len);
-	}
 	close(fd);
-	return (byte == len ? 1 : -1);
+	return (1);
 }
