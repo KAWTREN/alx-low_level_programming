fa73e7cee7280631c323682494b2c6a3abac1325
KAWTREN <kawtarennabirha@g,ail.com>
2023-08-07 14:19:26 -0700

    file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * append_text_to_file-appends text at the end of a file
+ * @filename: file name
+ * @text_content: text content
+ * Return 1 or -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, len;
+	ssize_t byte = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		len = 0;
+		while (text_content[len] != '\0')
+		{
+			len++;
+		}
+	}
+	fd = open(filename, O_WRONLY, O_APPEND);
+	byte = write(fd, text_content, len);
+	if (fd == -1 || byte == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
