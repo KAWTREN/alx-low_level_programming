7caa4b0ce0a9c0409f202529f2b730dcd24cee05
KAWTREN <kawtarennabirha@g,ail.com>
2023-08-07 14:26:06 -0700

    file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 		}
 	}
-	fd = open(filename, O_WRONLY, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	byte = write(fd, text_content, len);
 	if (fd == -1 || byte == -1)
 		return (-1);
