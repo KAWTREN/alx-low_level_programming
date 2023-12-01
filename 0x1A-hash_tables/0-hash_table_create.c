fcd398a48b6e7e5b4540105380211e8a759c7f0f
kawtar <kawtarennabirha@gmail.com>
2023-12-01 21:05:47 +0100

    c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,11 +5,13 @@
  * @size: integer
  * Return: pointer or NULL if somthing went wrong
 */
-hash_table_t *hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *new_table;
     unsigned long int i;
 
+    if(size == 0)
+        return(NULL);
     new_table = malloc(sizeof(hash_table_t));
     if(new_table == NULL)
         return (NULL);
@@ -24,5 +26,5 @@ hash_table_t *hash_table_create(unsigned long int size);
         new_table->array[i] = NULL;
     }
     new_table->size =  size;
-    return (new_table)
+    return (new_table);
 }
