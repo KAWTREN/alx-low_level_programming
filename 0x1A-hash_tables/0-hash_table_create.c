66b0dfd4911bf61e7d1d9d03dd001d7e3db4dd26
kawtar <kawtarennabirha@gmail.com>
2023-12-01 20:36:15 +0100

    c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,28 +1,30 @@
-#include"hash_tables.h"
+#include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: integer
- * Return: pointer or NULL if somthing went wrong
-*/
-hash_table_t *hash_table_create(unsigned long int size);
+ * hash_table_create - allocate memory for hash table
+ * @size: The size of the array
+ * Return: hash_table_t address or NULL if something wrong
+ */
+hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table;
-    unsigned long int i;
+    hash_table_t *hash_table;
+	unsigned int i;
 
-    new_table = malloc(sizeof(hash_table_t));
-    if(new_table == NULL)
-        return (NULL);
-    new_table->array = malloc(sizeof(hash_node_t *) * size);
-    if(new_table->array == NULL)
-    {
-        free(new_table);
-        return(NULL);
-    }
-    for(i = 0; i < size; i++)
-    {
-        new_table->array[i] = NULL;
-    }
-    new_table->size =  size;
-    return (new_table)
+	if (size == 0)
+		return (NULL);
+	hash_table = malloc(sizeof(hash_table_t *));
+	if (!hash_table)
+		return (NULL);
+
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t *) * (size + 1));
+	if (!hash_table->array)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
+	return (hash_table);
 }
